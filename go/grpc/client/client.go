d5bd49f2ce279535950714058c49a7a586491c98
Snehal Reddy Yelmati <snehalyelmati@gmail.com>
2022-03-30 23:12:37 +0530

    Go client update to handle REST and gRPC

--- go/grpc/client/client.go
+++ go/grpc/client/client.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"flag"
 	"log"
+	"net/http"
 	"strconv"
 	"time"
 
@@ -13,38 +15,58 @@ import (
 )
 
 const (
-	defaultName  = "world"
-	defaultCount = 1
-	defaultPort  = 50051
+	defaultName     = "world"
+	defaultCount    = 1
+	defaultPort     = 50051
+	defaultProtocol = "http"
 )
 
 var (
-	name  = flag.String("name", defaultName, "Name to greet")
-	count = flag.Int("count", defaultCount, "Number of requests")
-	port  = flag.Int("port", defaultPort, "Port")
+	name     = flag.String("name", defaultName, "Name to greet")
+	count    = flag.Int("count", defaultCount, "Number of requests")
+	port     = flag.Int("port", defaultPort, "Port")
+	protocol = flag.String("protocol", defaultProtocol, "Protocol, options 'grpc' or 'rest'")
 )
 
 func main() {
 	flag.Parse()
 
-	conn, err := grpc.Dial(":"+strconv.Itoa(*port), grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Fatalf("Did not connect: %v", err)
-	}
-	defer conn.Close()
-
-	c := pb.NewHelloClient(conn)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	start := time.Now().UnixMilli()
-	for i := 0; i < *count; i++ {
-		_, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
+	diff := 0.0
+	if *protocol == "grpc" {
+		conn, err := grpc.Dial(":"+strconv.Itoa(*port), grpc.WithTransportCredentials(insecure.NewCredentials()))
 		if err != nil {
-			log.Fatalf("Could not say hello: %v", err)
+			log.Fatalf("Did not connect: %v", err)
 		}
+		defer conn.Close()
+
+		c := pb.NewHelloClient(conn)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		start := float64(time.Now().UnixMilli())
+		for i := 0; i < *count; i++ {
+			_, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
+			if err != nil {
+				log.Fatalf("Could not say hello: %v", err)
+			}
+		}
+		diff = float64(time.Now().UnixMilli()) - start
 	}
-	diff := time.Now().UnixMilli() - start
-	log.Printf("Total time taken to run %d requests: %vms", *count, diff)
+
+	if *protocol == "http" {
+		data := []byte(`
+            "Name": "world"
+            `)
+		start := float64(time.Now().UnixMilli())
+		for i := 0; i < *count; i++ {
+            _, err := http.Post("http://127.0.0.1:"+strconv.Itoa(*port), "application/json; charset=utf-8", bytes.NewBuffer(data))
+			if err != nil {
+				log.Fatalf("Could not say hello(HTTP): %v", err)
+			}
+		}
+		diff = float64(time.Now().UnixMilli()) - start
+	}
+
+	log.Printf("Total time taken to run %d requests: %vms", *count, float64(diff)/float64(*count))
 }
