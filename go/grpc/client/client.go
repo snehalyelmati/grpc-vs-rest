81877858ba1ad549d541e2c2d20e0c2ae2e52eb4
Snehal Reddy Yelmati <snehalyelmati@gmail.com>
2022-03-31 00:44:46 +0530

    Go client update

--- go/grpc/client/client.go
+++ go/grpc/client/client.go
@@ -32,6 +32,7 @@ func main() {
 	flag.Parse()
 
 	diff := 0.0
+	responseTime := make(map[int]float64, 0)
 	if *protocol == "grpc" {
 		conn, err := grpc.Dial(":"+strconv.Itoa(*port), grpc.WithTransportCredentials(insecure.NewCredentials()))
 		if err != nil {
@@ -41,32 +42,45 @@ func main() {
 
 		c := pb.NewHelloClient(conn)
 
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 		defer cancel()
 
-		start := float64(time.Now().UnixMilli())
-		for i := 0; i < *count; i++ {
-			_, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
-			if err != nil {
-				log.Fatalf("Could not say hello: %v", err)
+		for i := 100; i <= *count; i += 50 {
+			start := float64(time.Now().UnixMilli())
+			for j := 0; j < i; j++ {
+				_, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
+				if err != nil {
+					log.Fatalf("Could not say hello: %v", err)
+				}
 			}
+			diff = float64(time.Now().UnixMilli()) - start
+
+			responseTime[i] = float64(diff) / float64(*count)
+			log.Printf("Average time taken to run %5d requests: %5v ms", i, responseTime[i])
+			time.Sleep(1 * time.Second)
 		}
-		diff = float64(time.Now().UnixMilli()) - start
 	}
 
 	if *protocol == "rest" {
 		data := []byte(`
             "Name": "world"
             `)
-		start := float64(time.Now().UnixMilli())
-		for i := 0; i < *count; i++ {
-            _, err := http.Post("http://127.0.0.1:"+strconv.Itoa(*port), "application/json; charset=utf-8", bytes.NewBuffer(data))
-			if err != nil {
-				log.Fatalf("Could not say hello(HTTP): %v", err)
+		for i := 100; i <= *count; i += 50 {
+			start := float64(time.Now().UnixMilli())
+			for j := 0; j < i; j++ {
+				_, err := http.Post("http://127.0.0.1:"+strconv.Itoa(*port), "application/json; charset=utf-8", bytes.NewBuffer(data))
+				if err != nil {
+					log.Fatalf("Could not say hello(HTTP): %v", err)
+				}
 			}
+			diff = float64(time.Now().UnixMilli()) - start
+
+			responseTime[i] = float64(diff) / float64(*count)
+			log.Printf("Average time taken to run %5d requests: %5v ms", i, responseTime[i])
+			time.Sleep(1 * time.Second)
 		}
-		diff = float64(time.Now().UnixMilli()) - start
 	}
 
-	log.Printf("Total time taken to run %d requests: %vms", *count, float64(diff)/float64(*count))
+	// fmt.Println("Response time:")
+	// fmt.Println(responseTime)
 }
